f08eb3cd565069327e33d6f7946fb97cfbabfc06
1SkyShadow1 <Eazymoja5@gmail.com>
2024-07-13 01:01:58 +0200

    chore: Refactor Card component for improved readability and maintainability

---
+++ frontend/src/app/ui/card.js
@@ -0,0 +1,52 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+
+const Card = ({ children, className, ...props }) => {
+  return (
+    <div
+      className={cn(
+        "rounded-md border border-border bg-card shadow-md",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+const CardContent = ({ children, className, ...props }) => {
+  return (
+    <div
+      className={cn("p-4", className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+const CardHeader = ({ children, className, ...props }) => {
+  return (
+    <div
+      className={cn("p-4 border-b border-border", className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+const CardFooter = ({ children, className, ...props }) => {
+  return (
+    <div
+      className={cn("p-4 border-t border-border", className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export { Card, CardContent, CardHeader, CardFooter };
