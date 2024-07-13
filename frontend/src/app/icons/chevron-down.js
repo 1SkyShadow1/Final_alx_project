582923c68c335e906d9947c49f9bcf9417eefd2c
JonathanNdawula <ndawjo23@gmail.com>
2024-07-14 00:09:03 +0300

    feat: cration of the chevron-down.js file containing  the ChevronDownIcon

---
+++ frontend/src/app/icons/chevron-down.js
@@ -0,0 +1,22 @@
+"use client"
+
+const ChevronDownIcon = (props) => {
+  return(
+    <svg
+        {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        >
+            <path d="M6 9 6 6 6-6"/>
+    </svg>  
+  );
+};
+
+export {ChevronDownIcon};
