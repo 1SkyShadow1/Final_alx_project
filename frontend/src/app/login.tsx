82392661981b33ba7e85026f271eb5dd4cdd9708
JonathanNdawula <ndawjo23@gmail.com>
2024-07-15 17:25:55 +0300

    refactor: Remove unused code for register user page

---
+++ frontend/src/app/login.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import {LoginFrom} from '@/app/auth/login-form';
+import {Header} from '@/app/header';
+
+const LoginPage = () =>{
+    return(
+      <div className='flex flex-col min-h-dvh'>
+       <Header/>
+        <main
+          className='flex-1 flex items-center py-12 md:py-24 lg:py-32'
+        >
+            <div className='container px-4 md:px-6'>
+              <LoginFrom/>
+            </div>
+        </main>
+      </div>    
+    );
+};
+
+export default LoginPage;
