a61d1dc2e79866ebe572050a20dc8384fb660d08
1SkyShadow1 <Eazymoja5@gmail.com>
2024-07-16 19:55:33 +0200

    refactor: Update database credentials and dependencies

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,41 @@
 /** @type {import('next').NextConfig} */
+import { resolve } from 'path';
+
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://localhost:3001/api/:path*',
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true,
+  swcMinify: true,
+  transpilePackages: ["final_alx_project/frontend/src/app/icons/"], 
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3001/api/:path*", // Replace 'http://localhost:3001' with your actual backend server URL
+      },
+    ];
+  },
+  // ... other configuration ...
+  compiler: {
+    styledComponents: true,
+  },
+  // No longer need experimental.appDir as it's enabled by default
+  // experimental: {
+  //   appDir: true,
+  // },
+  reactRoot: "concurrent", // Moved outside experimental 
+  // Include this block for the '@' alias 
+  // This will allow you to import files with a shorter path
+  // using `@` at the beginning of your import statements
+  // Example: import { cn } from '@/lib/utils';
+  webpack: (config, { isServer }) => { // Moved outside experimental 
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": resolve(__dirname), // Use 'resolve' from 'path' to get the absolute path
+      };
+    }
+
+    return config;
+  },
+};
+
+export default nextConfig;
